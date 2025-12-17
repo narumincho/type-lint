4fd8d279b04d21bbeee94d61b9c96767602aea75
narumincho <16481886+narumincho@users.noreply.github.com>
2025-12-17 13:52:05 +0900

    テストを改善

--- test.ts
+++ test.ts
@@ -1,4 +1,5 @@
 import { assertEquals } from "@std/assert";
+import { zip } from "@std/collections";
 import plugin from "./mod.ts";
 import example from "./example.ts" with { type: "text" };
 
@@ -9,47 +10,41 @@ Deno.test("no-array-shorthand", () => {
     example,
   );
 
-  assertEquals(diagnostics.length, 6);
-
-  const expectedErrors = [
-    {
-      message: "Use ReadonlyArray<T> instead of readonly T[]",
-      fixText: "ReadonlyArray<string>",
-      line: 1,
-    },
-    {
-      message: "Use ReadonlyArray<T> instead of readonly T[]",
-      fixText: "ReadonlyArray<string>",
-      line: 5,
-    },
-    {
-      message: "Use ReadonlyArray<T> instead of readonly T[]",
-      fixText: "ReadonlyArray<string>",
-      line: 5,
-    },
-    {
-      message: "Use ReadonlyArray<T> instead of readonly T[]",
-      fixText: "ReadonlyArray<string>",
-      line: 7,
-    },
-    {
-      message: "Use ReadonlyArray<T> instead of readonly T[]",
-      fixText: "ReadonlyArray<string>",
-      line: 7,
-    },
-    {
-      message: "Use Array<T> instead of T[]",
-      fixText: "Array<number>",
-      line: 15,
-    },
-  ];
-
-  for (const [i, d] of diagnostics.entries()) {
-    assertEquals(d.id, "my-plugin/no-array-shorthand");
-    assertEquals(d.message, expectedErrors[i].message);
-    const fix = d.fix?.[0];
-    if (fix && expectedErrors[i].fixText) {
-      assertEquals(fix.text, expectedErrors[i].fixText);
-    }
-  }
+  assertEquals(diagnostics, [{
+    id: "my-plugin/no-array-shorthand",
+    message: "Use ReadonlyArray<T> instead of readonly T[]",
+    hint: undefined,
+    range: [13, 30],
+    fix: [{ range: [13, 30], text: "ReadonlyArray<string>" }],
+  }, {
+    id: "my-plugin/no-array-shorthand",
+    message: "Use ReadonlyArray<T> instead of readonly T[]",
+    hint: undefined,
+    range: [68, 85],
+    fix: [{ range: [68, 85], text: "ReadonlyArray<string>" }],
+  }, {
+    id: "my-plugin/no-array-shorthand",
+    message: "Use ReadonlyArray<T> instead of readonly T[]",
+    hint: undefined,
+    range: [89, 106],
+    fix: [{ range: [89, 106], text: "ReadonlyArray<string>" }],
+  }, {
+    id: "my-plugin/no-array-shorthand",
+    message: "Use ReadonlyArray<T> instead of readonly T[]",
+    hint: undefined,
+    range: [170, 187],
+    fix: [{ range: [170, 187], text: "ReadonlyArray<string>" }],
+  }, {
+    id: "my-plugin/no-array-shorthand",
+    message: "Use ReadonlyArray<T> instead of readonly T[]",
+    hint: undefined,
+    range: [150, 167],
+    fix: [{ range: [150, 167], text: "ReadonlyArray<string>" }],
+  }, {
+    id: "my-plugin/no-array-shorthand",
+    message: "Use Array<T> instead of T[]",
+    hint: undefined,
+    range: [349, 357],
+    fix: [{ range: [349, 357], text: "Array<number>" }],
+  }]);
 });
